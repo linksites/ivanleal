3aa8c53456ff8e0cc88ded4116e628271ac46f2c
linksites <sergios2@ymail.com>
2026-05-27 23:04:44 -0300

    Update SEO title location

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,5 +1,5 @@
 export const siteMetadata = {
-  title: 'Ivan Leal JJ | Mestre de Jiu-Jitsu em Lisboa',
+  title: 'Ivan Leal JJ | Mestre de Jiu-Jitsu em Portugal',
   description:
     'Site institucional do mestre e atleta Ivan Leal JJ: Jiu-Jitsu de pressão, técnica e legado, com conquistas, galeria, vídeos e seminários.',
   url: 'https://ivanlealjj.com',
